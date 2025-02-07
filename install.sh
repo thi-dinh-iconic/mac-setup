6da94d5189ccbf90ede2ef5149449d13638650b1
Thi <thi.dinh@theiconic.com.au>
2025-02-07 14:52:42 +0700

    add scritps

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install pyenv dependencies and pyenv
+brew install openssl readline sqlite3 xz zlib
+brew install pyenv
+
+cp .zshrc ~/.zshrc
+# Add pyenv to shell
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# Reload shell configuration
+source ~/.zshrc
+
+# Install Python 3.13.2 using pyenv
+pyenv install 3.13.2
+pyenv global 3.13.2
+
+# Install Ansible
+brew install ansible
+
+# Install Ansible requirements
+ansible-galaxy install -r requirements.yml
+
+# Run Ansible playbook
+ansible-playbook -i inventory.yml default.yml
+
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg" && sudo installer -pkg AWSCLIV2.pkg -target /
+
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+brew install fzf 
+brew install gimme-aws-creds
+brew install awscli
+brew install terraform
+brew install kubectl
+brew install kubie
+brew install --cask snipaste
+brew install --cask iterm2
+brew install --cask 1password
+brew install --cask 1password-cli
+brew install --cask gotiengviet
+brew install --cask cursor
+brew install --cask singlebox
+sudo -H pip install credstash
